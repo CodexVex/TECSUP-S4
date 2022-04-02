d681655d80e5e17a0f5be3d6d2c76f43121ee588
Victoria Lavarello <vickymoon1509@gmail.com>
2022-04-02 16:21:47 -0500

    JS-todos_los_bloques

--- index.js
+++ index.js
@@ -28,3 +28,24 @@ class Block{
     }
 }
 
+//todos los bloques
+
+const blocks = [
+    new Block (10,350),
+    new Block (100,350),
+    new Block (190,350),
+    new Block (280,350),
+    new Block (370,350),
+    new Block (10,320),
+    new Block (100,320),
+    new Block (190,320),
+    new Block (280,320),
+    new Block (370,320),
+    new Block (10,290),
+    new Block (100,290),
+    new Block (190,290),
+    new Block (280,290),
+    new Block (370,290),
+]
+
+//dibujando los bloques
